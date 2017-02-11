47254421c5465c377f5544a1c2fbd28cc685cdb8
Amantel <amantels@gmail.com>
2017-02-11 23:24:36 +0300

    Clusterize by path and by uri in one array
    Fixed remarks, used jshint, removed clusterize call, moved to one file

--- futurator_on_page.js
+++ futurator_on_page.js
@@ -43,4 +43,137 @@ function clusterize() {
   imgClusters.forEach(clust => console.log(clust.toString()));
 }
 
-clusterize();
+//clusterize();
+
+/* DomPath from stackoverflow http://stackoverflow.com/a/16742828 */
+function getDomPath(el) {
+    if (!el) {
+        return false;
+   }
+    var stack = [];
+    while (el.parentNode !== null) {
+        var nodeName = el.nodeName.toLowerCase();
+        stack.unshift(nodeName);
+		el = el.parentNode;
+		if (el.nodeType === 11) { // for shadow dom
+			el = el.host;
+		}        
+    }
+    stack.splice(0, 1); // removes the html element
+    return stack.join(' > ');
+}
+ 
+/*********FUNCTIONS********/
+Object.prototype.addToKey = function (key, value) {
+    if (key in this)
+        this[key].push(value);
+    else
+        this[key] = [value];
+};
+function log(obj, debug) {
+    if (debug) {
+        console.log(obj);
+    }
+}
+
+function getPathAndUrlsArrayfromAnchors(debug) {
+    //vars
+    //return object
+    var resultObj = {};
+    //array of all anchors with "good" hrefs, html and DOM paths
+    var anchorsArray = [];
+    //object with key = common DOM path, value = array of anchors in this DOM path
+    var anchorsByPathArrayObj = {};
+    //object with key= distinct DOM path, value = distinct URItemplates 
+    var uriClusterByPathObj = {};
+    //get clusters
+    var linkClusters = findUriClusters([].slice.call(document.links).map(a=>a.href));
+
+    //Collect all anchors in DOM
+    var anchors = document.querySelectorAll("a");
+    //Populate anchorsArray with elements
+    anchors.forEach(function (el, index) {
+        var shortHref;       
+        var href = el.href;
+        if(href !== undefined) {
+            var idx = href.indexOf('#');
+            if (idx >= 0)
+                shortHref = href.slice(0, idx);
+            else
+                shortHref = href;
+
+            if (shortHref !== "") {
+                var anchorObj = {};
+                anchorObj.href = shortHref;
+                anchorObj.html = el.innerHTML; // for tests
+                anchorObj.path = getDomPath(el);
+                anchorsArray.push(anchorObj);
+            }
+        }
+        
+    });
+    //add to anchorsArray elements linkClusters, based on element.href vs 
+    //each cluster regex
+    anchorsArray.forEach(function (el) {
+        el.linkClusters = [];
+        linkClusters.forEach(function (cluster) {
+            var expr = '^' + cluster.toRegexp() + "$";
+            if (el.href.match(expr)) {
+                el.linkClusters.push(cluster);
+            }
+        });
+    });
+    //populating object with key = common DOM path, 
+    //value = array of anchors in this DOM path
+    anchorsArray.forEach(function (anchorObj) {
+        anchorsByPathArrayObj.addToKey(anchorObj.path, anchorObj);
+    });
+    //Now in anchorsByPathArrayObj we have all distinct paths with anchors
+    log(anchorsByPathArrayObj, debug);
+    //Look inside each distinct path in anchorsByPathArrayObj and
+    //find findUriClusters (>0) there. Store them
+    Object.keys(anchorsByPathArrayObj).forEach(function (path) { 
+        var clusters = findUriClusters(        
+            anchorsByPathArrayObj[path]?anchorsByPathArrayObj[path].map(anchorObj =>
+                anchorObj.href):[]        
+        );
+        if (clusters.length > 0) {
+            uriClusterByPathObj[path] = clusters;
+        }
+    });
+    //Now we have all distinct paths with 1 or more UriClusters.
+    log(uriClusterByPathObj, debug);
+    Object.keys(anchorsByPathArrayObj).forEach(function (path) {
+        anchorsByPathArrayObj[path].forEach(function (anchorObj) {
+            anchorObj.linkClusters = anchorObj.linkClusters.filter(cluster =>                 
+                (uriClusterByPathObj[path]?uriClusterByPathObj[path]
+                    .map(pathCluster => pathCluster.path):[])
+                        .indexOf(cluster.path) >= 0
+            );
+        });
+    });
+    //Now in anchorsByPathArrayObj we have all distinct paths with anchors
+    //and with right UriClusters.
+    //In short we had now there are UriClasters in anchors
+    //that are equal instide path
+    log(anchorsByPathArrayObj, debug);
+
+    //Add to result objects with distinct path and cluster 
+    Object.keys(anchorsByPathArrayObj).forEach(function (path) {
+        var urlClusterArray = anchorsByPathArrayObj[path];
+        urlClusterArray.forEach(function (anchorObj) {
+            anchorObj.linkClusters.forEach(function (cluster) {
+                resultObj.addToKey("path: " + path + " | cluster: " + cluster.path, anchorObj);
+            });
+        });
+    });
+
+    return resultObj;
+}
+
+
+var debug=true;
+
+var pathAndUrlsArrayfromAnchors = getPathAndUrlsArrayfromAnchors(debug);
+log("result", debug);
+log(pathAndUrlsArrayfromAnchors, debug);
